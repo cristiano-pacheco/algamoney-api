c85801e3c9c0e01f709014884fd1fafae31c1ece
cristianopacheco <chris.spb25@gmail.com>
2019-05-26 21:56:26 -0300

    5.8. Desafio: Removendo lançamentos

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -12,12 +12,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
@@ -62,6 +64,12 @@ public class LancamentoResource {
 	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
 		Lancamento Lancamento = lancamentoRepository.findOne(codigo);
 		return Lancamento != null ? ResponseEntity.ok(Lancamento) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		lancamentoRepository.delete(codigo);
 	}
 	
 	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
